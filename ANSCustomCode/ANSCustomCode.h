bbe60432ee5a10cfb16740f07a166abbc0b8ebc3
Tuan Nghia Nguyen <nghia.nguyen@anscenter.com>
2024-12-06 11:48:20 +1100

    Update Object format

--- ANSCustomCode/ANSCustomCode.h
+++ ANSCustomCode/ANSCustomCode.h
@@ -12,7 +12,7 @@ struct CustomObject
     std::string className{};
     float confidence{ 0.0 };
     cv::Rect box{};
-    std::vector<cv::Point> polygon;
+    std::vector<cv::Point2f> polygon;
     cv::Mat mask;             //Json string mask ="point1.x,point1.y,...."
     std::vector<float> kps{};   // Pose exsimate keypoint
     std::string extraInfo;      // More information such as facial recognition
